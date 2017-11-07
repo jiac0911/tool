e51000f0a3e43c077dfd6cd8781fab65d0602860
jiac <jiac0911@hotmail.com>
2017-11-07 17:36:16 -0500

    index

---
+++ getExcel.php
@@ -0,0 +1,29 @@
+<?php
+
+function importData($bias, $ruta){
+    $row = 1;
+    $datos=array();
+    $deseados=array();
+
+    if (($handle = fopen($ruta, "r")) !== FALSE) {
+        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+            $num = count($data);
+            // echo "<p> $num fields in line $row: <br /></p>\n";
+            for ($c=0; $c < $num; $c++) {
+                // echo $data[$c] . "<br />\n";
+                $datos[$row][$c]=(float)$data[$c];
+            }
+            $temp=array_pop($datos[$row]);
+            array_push($datos[$row], (float)$bias);
+            array_push($datos[$row],$temp);
+            $row++;
+        }
+        fclose($handle);
+    }
+    return $datos;
+
+}
+
+$datos=importData(0,'test.csv');
+
+var_dump($datos);
---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+<title>W3.CSS</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="w3.css">
+<link rel="stylesheet" href="index.css">
+<body>
+
+<!-- Sidebar -->
+<div class="w3-sidebar w3-light-grey w3-bar-block" style="width:25%">
+  <h3 id="tituMen" class="w3-bar-item">Redes</h3>
+  <a href="#" class="w3-bar-item w3-button">Perceptron</a>
+  <a href="#" class="w3-bar-item w3-button">Adaline</a>
+  <a href="#" class="w3-bar-item w3-button">Perceptron Multicapa</a>
+  <a href="#" class="w3-bar-item w3-button">Madaline</a>
+  <a href="#" class="w3-bar-item w3-button">Base Radial</a>
+  <a href="#" class="w3-bar-item w3-button">Evolutivo</a>
+</div>
+
+<!-- Page Content -->
+<div style="margin-left:25%">
+
+<div class="w3-container w3-teal">
+  <h1>Nota:</h1>
+</div>
+
+<img id="ima" src="cinco.jpg" alt="Car" >
+
+</div>
+
+</body>
+</html>
