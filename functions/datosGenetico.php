7e2fc5dc99f14d43e0b62257909e18bb08aef28e
jiac <jiac0911@hotmail.com>
2017-11-19 19:37:11 -0500

    genetico malo

--- functions/datosGenetico.php
+++ functions/datosGenetico.php
@@ -1,5 +1,5 @@
 <?php
-set_time_limit ( 600 );
+set_time_limit ( 6000 );
 require_once('../functions/genetico.php');
 require_once('../functions/individuo.php');
 require_once('../functions/perceptronGenetico.php');
@@ -15,7 +15,7 @@ if(isset($_POST["nroIte"])){
 
 // var_dump($nroIte);
 // die();
-$nroIte=10000;
+$nroIte=100;
 
 for ($j=0; $j <100 ; $j++) {
 
@@ -32,9 +32,31 @@ for ($j=0; $j <100 ; $j++) {
 $individuos=$padres;
 
 $i=0;
-while ($i<=$nroIte || ($individuos->getEcm()>=0.0001) {
+while ($i<$nroIte || ($individuos[0]->getEcm()>=0.0001)) {
     $individuos=ruleta($individuos);
+
     $i+=1;
+
 }
 
+        $datos=array(
+        array(0,0,0,0),
+        array(0,1,0,1),
+        array(1,0,0,1),
+        array(1,1,0,1), );
+
+for ($i=0; $i <100 ; $i++) {
+
+        $genes=$individuos[$i]->genes;
+        $perceptron =new perceptronGenetico(2,4,0.5,1,$genes);
+        $perceptron->train($datos);
+        $ecm= $perceptron->ecm;
+        $individuos[$i]->setEcm($ecm);
+        $aptitud=(1/$ecm);
+        $individuos[$i]->setAptitud($aptitud);
+    }
+
+
+var_dump($individuos);
+die();
 ?>
--- functions/genetico.php
+++ functions/genetico.php
@@ -5,22 +5,24 @@ include_once 'perceptronGenetico.php';
 include_once 'individuo.php';
 
 function ruleta($individuos){
-
+    // var_dump($individuos);
     $sumaAptitud=0;
     $aptitudAcum=0;
     $hijos;
+    $solved=0;
 
         $datos=array(
         array(0,0,1,0),
-        array(0,1,1,1),
-        array(1,0,1,1),
-        array(1,1,1,1), );
+        array(1,1,1,1),
+        array(1,0,1,0),
+        array(0,1,1,0),
+        );
 
 
     for ($i=0; $i <100 ; $i++) {
 
         $genes=$individuos[$i]->genes;
-        $perceptron =new perceptronGenetico(2,4,0.5,1,$genes);
+        $perceptron =new perceptronGenetico(2,4,0.1,1,$genes);
         $perceptron->train($datos);
         $ecm= $perceptron->ecm;
         $individuos[$i]->setEcm($ecm);
@@ -32,14 +34,17 @@ function ruleta($individuos){
 
          }
         else{
+
             $aptitud=(1/$ecm);
             $individuos[$i]->setAptitud($aptitud);
             $sumaAptitud=$sumaAptitud+$aptitud;
+            $solved=1;
             $aptitud=$individuos[$i]->getAptitud();
             for ($j=0; $j <100 ; $j++) {
                $individuos[$j]= $individuos[$i];
             }
-
+            $i=100;
+            // var_dump($individuos[$i]);
         }
 
     }
@@ -53,38 +58,46 @@ function ruleta($individuos){
         $individuos[$j]->setAptitudAcum($aptitudAcum);
 
     }
-
+    // var_dump($individuos);
     for ($k=0; $k <100 ; $k++) {
         $p=0;
         $m=0;
         $aleatorioP=mt_rand() / mt_getrandmax();
         $aleatorioM=mt_rand() / mt_getrandmax();
-
+        // $aleatorioP=0;
+        // $aleatorioM=0;
 
         while ($aleatorioP >= $individuos[$p]->getAptitudAcum()) {
             $p++;
+
         }
+        // echo $p."\r\n";
         while ($aleatorioM >= $individuos[$m]->getAptitudAcum()) {
             $m++;
         }
-
+        // var_dump($individuos);
         $x=mt_rand() / mt_getrandmax();
-        $vectorGenes[0]=$individuos[$p]->genes[0]*$x+(1-$individuos[$m]->genes[0]);
-        $vectorGenes[1]=$individuos[$p]->genes[1]*$x+(1-$individuos[$m]->genes[1]);
-        $vectorGenes[2]=$individuos[$p]->genes[2]*$x+(1-$individuos[$m]->genes[2]);
-        $mut=1;
-        if ($mut<=20) {
-            $vectorGenes[0]=$vectorGenes[0]*(rand(-1,1));
-            $vectorGenes[1]=$vectorGenes[1]*(rand(-1,1));
-            $vectorGenes[2]=$vectorGenes[2]*(rand(-1,1));
-            $mut+=1;
-        }
+        $y=mt_rand() / mt_getrandmax();
+        $z=mt_rand() / mt_getrandmax();
+        $vectorGenes[0]=$individuos[$p]->genes[0]*$x+((1-$x)*($individuos[$m]->genes[0]));
+        $vectorGenes[1]=$individuos[$p]->genes[1]*$y+((1-$y)*($individuos[$m]->genes[1]));
+        $vectorGenes[2]=$individuos[$p]->genes[2]*$z+((1-$z)*($individuos[$m]->genes[2]));
+        // var_dump($vectorGenes);
+        // $mut=1;
+        // if ($mut<=20 && $solved!=1) {
+        //     $vectorGenes[0]=$vectorGenes[0]+mt_rand() / mt_getrandmax()*2-1;;
+        //     $vectorGenes[1]=$vectorGenes[1]+mt_rand() / mt_getrandmax()*2-1;;
+        //     $vectorGenes[2]=$vectorGenes[2]+mt_rand() / mt_getrandmax()*2-1;;
+        //     $mut+=1;
+        // }
         $hijos[$k]=new Individuo($vectorGenes);
 
 
 
     }
+    // echo "           otro:    ";
     $individuos=$hijos;
+    // var_dump($individuos);
     return $individuos;
 
 }
